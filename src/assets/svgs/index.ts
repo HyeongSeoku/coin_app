f5be04c3be99307d1705792d22ad9f2e9f4a1fdd
HyeongSeoku <gudtjr3437@naver.com>
2022-06-03 03:25:58 +0900

    Chore : Add coin svgs

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,1 +1,4 @@
 export { ReactComponent as Logo } from './logo.svg'
+export { ReactComponent as Bitcoin } from './bitcoinLogo.svg'
+export { ReactComponent as Up } from './up.svg'
+export { ReactComponent as Down } from './down.svg'
