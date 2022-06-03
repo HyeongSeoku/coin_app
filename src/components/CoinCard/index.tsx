fb95064d0fb1b9be4c001cbbfa0a04d5f85542fc
HyeongSeoku <gudtjr3437@naver.com>
2022-06-04 03:35:54 +0900

    Feat : auto change coin icon

--- src/components/CoinCard/index.tsx
+++ src/components/CoinCard/index.tsx
@@ -1,4 +1,9 @@
-import { Bitcoin, Down, Up } from 'assets/svgs'
+import { Down, Up } from 'assets/svgs'
+import { COIN_ICON, DEFAULT_COIN_ICON } from 'constants/icons'
+import { transformNumber } from 'utils/transformNumber'
+
+import cx from 'classnames'
+
 import styles from './coinCard.module.scss'
 
 interface ICoinCard {
@@ -10,23 +15,26 @@ interface ICoinCard {
 
 const CoinCard = ({ coinData }: { coinData: ICoinCard }) => {
   const { name, symbol, price, market_cap_change_24h: marketCapChange24h } = coinData
-
-  const varianceIcon = marketCapChange24h < 0 ? <Down /> : <Up />
+  const [translatePrice, unit] = transformNumber(price)
+  const isIncrease = marketCapChange24h > 0
+  const varianceIcon = isIncrease ? <Up /> : <Down />
+  const coinLogo = COIN_ICON[symbol] || DEFAULT_COIN_ICON
 
   return (
     <li className={styles.cardContainer}>
-      <div className={styles.imgContainer}>
-        <Bitcoin />
-      </div>
+      <div className={styles.imgContainer}>{coinLogo}</div>
       <div className={styles.titlContainer}>
         <span className={styles.nameText}>{name}</span>
         <span className={styles.symbolText}>{symbol}</span>
       </div>
       <div className={styles.priceContainer}>
-        <span className={styles.priceText}>{price}</span>
-        <div>
-          <span>{varianceIcon}</span>
-          <span className={styles.marketCapText}>{marketCapChange24h}</span>
+        <span className={styles.priceText}>
+          {translatePrice}
+          {unit}
+        </span>
+        <div className={styles.marketCapContainer}>
+          <span className={styles.varianceIcon}>{varianceIcon}</span>
+          <span className={cx(styles.marketCapText, { [styles.increase]: isIncrease })}>{marketCapChange24h}%</span>
         </div>
       </div>
     </li>
