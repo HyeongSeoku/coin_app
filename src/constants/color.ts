47caf6be20a3548a796a9ade2a964b3e6ed96789
HyeongSeoku <gudtjr3437@naver.com>
2022-06-04 03:35:12 +0900

    Feat : Coin icon

---
+++ src/constants/color.ts
@@ -0,0 +1,4 @@
+export const COLOR = {
+  DOWN: '#BB0500',
+  UP: '#38a45c',
+}
---
+++ src/constants/icons.tsx
@@ -0,0 +1,91 @@
+import {
+  Atom,
+  Avax,
+  Bitcoin,
+  BNB,
+  Btcb,
+  Busd,
+  Cardano,
+  ChainLink,
+  Cro,
+  Dai,
+  DefaultCoin,
+  Dogecoin,
+  Dot,
+  Ethereum,
+  Hex,
+  Ltc,
+  Matic,
+  Near,
+  Paxg,
+  Shib,
+  Solana,
+  Thr,
+  Trx,
+  Uni,
+  Usdc,
+  Usdt,
+  Wbtc,
+  Xrp,
+} from 'assets/svgs'
+
+interface ICoinProps {
+  BTC: JSX.Element
+  ETH: JSX.Element
+  USDT: JSX.Element
+  BNB: JSX.Element
+  USDC: JSX.Element
+  XRP: JSX.Element
+  HEX: JSX.Element
+  ADA: JSX.Element
+  BUSD: JSX.Element
+  SOL: JSX.Element
+  DOGE: JSX.Element
+  SHIB: JSX.Element
+  WBTC: JSX.Element
+  AVAX: JSX.Element
+  DOT: JSX.Element
+  MATIC: JSX.Element
+  CRO: JSX.Element
+  DAI: JSX.Element
+  LTC: JSX.Element
+  ATOM: JSX.Element
+  LINK: JSX.Element
+  NEAR: JSX.Element
+  UNI: JSX.Element
+  TRX: JSX.Element
+
+  [key: string]: any
+}
+
+export const COIN_ICON: ICoinProps = {
+  BTC: <Bitcoin />,
+  ETH: <Ethereum />,
+  USDT: <Usdt />,
+  BNB: <BNB />,
+  USDC: <Usdc />,
+  XRP: <Xrp />,
+  HEX: <Hex />,
+  ADA: <Cardano />,
+  BUSD: <Busd />,
+  SOL: <Solana />,
+  DOGE: <Dogecoin />,
+  SHIB: <Shib />,
+  WBTC: <Wbtc />,
+  AVAX: <Avax />,
+  DOT: <Dot />,
+  MATIC: <Matic />,
+  CRO: <Cro />,
+  DAI: <Dai />,
+  LTC: <Ltc />,
+  ATOM: <Atom />,
+  LINK: <ChainLink />,
+  NEAR: <Near />,
+  UNI: <Uni />,
+  TRX: <Trx />,
+  BTCB: <Btcb />,
+  THR: <Thr />,
+  PAXG: <Paxg />,
+}
+
+export const DEFAULT_COIN_ICON = <DefaultCoin />
