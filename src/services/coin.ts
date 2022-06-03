d34149c9883bbd32a8c61ebd195130205f83e2e1
HyeongSeoku <gudtjr3437@naver.com>
2022-06-04 03:34:12 +0900

    Feat : api module about coin

--- src/services/coin.ts
+++ src/services/coin.ts
@@ -1,9 +1,5 @@
 import axios from 'axios'
 
-interface ITickerParams {
-  quotes: string
-}
-
 interface IHistoryParams {
   coinId: string
   start: string
@@ -12,15 +8,23 @@ interface IHistoryParams {
 
 const COIN_BASE_URL = 'https://api.coinpaprika.com'
 
-export const getCoinList = () => axios.get(`${COIN_BASE_URL}/v1/coins`)
-
-export const getCoinTickers = (params: ITickerParams) =>
-  axios.get(`${COIN_BASE_URL}/v1/tickers`, {
-    params: { ...params },
+export const getKeywordCoins = () =>
+  axios.get(`/v1/coins`).then((res) => {
+    const result: IDropDown[] = []
+    res.data.map((item: ICoinData) => result.push({ name: item.name, id: item.id }))
+    return result
   })
 
+export const getTop100CoinList = () => axios.get(`v1/coins`).then((res) => res.data.splice(0, 100))
+
+export const getCoinList = () => axios.get(`v1/coins`)
+
+export const getTopp100CoinTickers = () => axios.get(`v1/tickers?quotes=KRW`).then((res) => res.data.splice(0, 100))
+
+export const getCoinTickers = () => axios.get(`v1/tickers?quotes=KRW`)
+
 export const getCoinDetail = (params: IHistoryParams) =>
-  axios.get(`${COIN_BASE_URL}/v1/tickers/${params.coinId}/historical`, {
+  axios.get(`v1/tickers/${params.coinId}/historical`, {
     params: {
       start: params.start,
       interval: params.interval,
