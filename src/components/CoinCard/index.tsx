499cebfd313a289fa7d1e8d0c93f03bbf3548761
HyeongSeoku <gudtjr3437@naver.com>
2022-06-05 04:48:37 +0900

    Feat : Change coin card UI & add coin icons

--- src/components/CoinCard/index.tsx
+++ src/components/CoinCard/index.tsx
@@ -1,24 +1,39 @@
-import { Down, Up } from 'assets/svgs'
+import { DownIcon, NotChangeIcon, UpIcon } from 'assets/svgs'
 import { COIN_ICON, DEFAULT_COIN_ICON } from 'constants/icons'
 import { transformNumber } from 'utils/transformNumber'
 
 import cx from 'classnames'
 
 import styles from './coinCard.module.scss'
+import { useMemo } from 'react'
 
 interface ICoinCard {
   name: string
   symbol: string
   price: number
-  market_cap_change_24h: number
+  percentChange24h: number
 }
 
 const CoinCard = ({ coinData }: { coinData: ICoinCard }) => {
-  const { name, symbol, price, market_cap_change_24h: marketCapChange24h } = coinData
+  const { name, symbol, price, percentChange24h } = coinData
   const [translatePrice, unit] = transformNumber(price)
-  const isIncrease = marketCapChange24h > 0
-  const varianceIcon = isIncrease ? <Up /> : <Down />
   const coinLogo = COIN_ICON[symbol] || DEFAULT_COIN_ICON
+
+  const [varianceText, varianceIcon] = useMemo(() => {
+    let icon = <NotChangeIcon />
+    let text = 'equl'
+    if (!percentChange24h) return [text, icon]
+
+    if (percentChange24h > 0) {
+      icon = <UpIcon />
+      text = 'up'
+      return [text, icon]
+    }
+
+    icon = <DownIcon />
+    text = 'down'
+    return [text, icon]
+  }, [percentChange24h])
 
   return (
     <li className={styles.cardContainer}>
@@ -29,12 +44,20 @@ const CoinCard = ({ coinData }: { coinData: ICoinCard }) => {
       </div>
       <div className={styles.priceContainer}>
         <span className={styles.priceText}>
-          {translatePrice}
+          ${translatePrice}
           {unit}
         </span>
         <div className={styles.marketCapContainer}>
           <span className={styles.varianceIcon}>{varianceIcon}</span>
-          <span className={cx(styles.marketCapText, { [styles.increase]: isIncrease })}>{marketCapChange24h}%</span>
+          <span
+            className={cx(
+              styles.marketCapText,
+              { [styles.increase]: varianceText === 'up' },
+              { [styles.decrease]: varianceText === 'down' }
+            )}
+          >
+            {percentChange24h}%
+          </span>
         </div>
       </div>
     </li>
