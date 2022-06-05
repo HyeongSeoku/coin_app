31923759a7231c1eaf3f6bf5558d5ee31f9efd98
HyeongSeoku <gudtjr3437@naver.com>
2022-06-05 16:05:41 +0900

    Fix : file error

--- src/routes/Detail/index.tsx
+++ src/routes/Detail/index.tsx
@@ -14,10 +14,10 @@ interface IProps {
 }
 
 interface IGraphProps {
- timestamp: string
- price: number 
- volume_24h: number 
- market_cap: number 
+  timestamp: string
+  price: number
+  volume_24h: number
+  market_cap: number
 }
 
 const Detail = ({ idState }: IProps) => {
@@ -33,10 +33,7 @@ const Detail = ({ idState }: IProps) => {
 
   const { data: detailChartData } = useQuery(
     [`#chartData${detailData.id}`],
-    () =>
-      getCoinDetail({ coinId: detailData.id, start: apiStart, interval: '1h' }).then((res) => 
-        res.data.map((item: IGraphProps) => {item.timestamp,...item})
-      ),
+    () => getCoinDetail({ coinId: detailData.id, start: apiStart, interval: '1h' }),
     {
       refetchOnWindowFocus: false,
       suspense: true,
