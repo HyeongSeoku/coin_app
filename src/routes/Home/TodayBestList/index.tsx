b7b7e543aea48e7175c467749c396340c6fa20ae
HyeongSeoku <gudtjr3437@naver.com>
2022-06-08 19:14:46 +0900

    Refactor : Delete useless code

--- src/routes/Home/TodayBestList/index.tsx
+++ src/routes/Home/TodayBestList/index.tsx
@@ -1,26 +1,22 @@
 import CoinCard from 'components/CoinCard'
 import { sortBy } from 'lodash'
-import { useEffect, useMemo } from 'react'
+import { useMemo } from 'react'
 import { useRecoilValue } from 'recoil'
 import { coinTickerListState } from 'states/coin'
 import Toggle from 'components/Toggle'
 import { HOME_TOGGLE } from 'constants/home'
 
 import styles from './todayBestList.module.scss'
-import { homeToggleState } from 'states/home'
+
+// TODO: homeToggleState로 토글했을때 보여줄 컨텐츠 제어
 
 const TodayBestList = () => {
   const coinTickerList = useRecoilValue(coinTickerListState)
-  const homeSelectedToggle = useRecoilValue(homeToggleState)
 
   const toDayBest4List = useMemo(() => {
     const arr = sortBy(coinTickerList, `quotes.USD.percent_change_24h`).reverse()
     return arr.slice(0, 4)
   }, [coinTickerList])
-
-  useEffect(() => {
-    console.log(homeSelectedToggle)
-  }, [homeSelectedToggle])
 
   return (
     <div className={styles.container}>
@@ -34,7 +30,7 @@ const TodayBestList = () => {
                 name: coinData.name,
                 symbol: coinData.symbol,
                 price: coinData.quotes.USD.price,
-                percentChange24h: coinData.quotes.USD.percent_change_24h,
+                percentChange1h: coinData.quotes.USD.percent_change_1h,
               }}
             />
           ))}
