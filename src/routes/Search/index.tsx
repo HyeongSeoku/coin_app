edd9859e4093f8a95c1f4dc08c3ee0050a061041
HyeongSeoku <gudtjr3437@naver.com>
2022-06-12 02:29:14 +0900

    Feat : add detail component in search page

--- src/routes/Search/index.tsx
+++ src/routes/Search/index.tsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from 'react-router-dom'
+import { useSearchParams, useParams } from 'react-router-dom'
 import { useMount } from 'react-use'
 import { useSetRecoilState } from 'recoil'
 
@@ -7,14 +7,27 @@ import SearchForm from 'routes/Home/SearchForm'
 import { dropDownOpenState } from 'states/search'
 
 import styles from './search.module.scss'
-import { Suspense } from 'react'
+import { Suspense, useMemo } from 'react'
 import Loader from 'components/Loader'
 import DefaultList from './DefaultList'
+import ResultList from './ResultList'
+import Detail from 'routes/Search/Detail'
 
 const SearchPage = () => {
   const [searchParam] = useSearchParams()
   const currentKeyword = searchParam.get('name')
   const setDropDownState = useSetRecoilState(dropDownOpenState)
+  const { coinId } = useParams()
+
+  const isDetail = useMemo(() => {
+    if (!coinId) return false
+    return true
+  }, [coinId])
+
+  const searchResultList = useMemo(() => {
+    if (!currentKeyword) return <DefaultList />
+    return <ResultList keyword={currentKeyword} />
+  }, [currentKeyword])
 
   useMount(() => {
     setDropDownState(false)
@@ -22,14 +35,20 @@ const SearchPage = () => {
 
   return (
     <div>
-      <Suspense fallback={<Loader />}>
-        <div className={styles.searchContainer}>
-          <SearchForm />
-          <DropDownList />
-        </div>
-        <div>{currentKeyword}</div>
-        {!currentKeyword && <DefaultList />}
-      </Suspense>
+      {!isDetail && (
+        <Suspense fallback={<Loader />}>
+          <div className={styles.searchContainer}>
+            <SearchForm />
+            <DropDownList />
+          </div>
+          <div>{searchResultList}</div>
+        </Suspense>
+      )}
+      {isDetail && (
+        <Suspense fallback={<Loader />}>
+          <Detail idState={coinId!} />
+        </Suspense>
+      )}
     </div>
   )
 }
