45504e7e93c20b91b560730a12157539e43b39d9
HyeongSeoku <gudtjr3437@naver.com>
2022-06-12 02:28:16 +0900

    Design : hometoggle style

--- src/routes/Home/HomeToggle/index.tsx
+++ src/routes/Home/HomeToggle/index.tsx
@@ -2,9 +2,10 @@ import Toggle from 'components/Toggle'
 import { HOME_TOGGLE } from 'constants/home'
 import { useMemo, useState } from 'react'
 
-import styles from './homeToggle.module.scss'
 import MyFeed from './MyFeed'
 import TodayBestList from './TodayBestList'
+
+import styles from './homeToggle.module.scss'
 
 const HomeToggle = () => {
   const [selectedToggle, setSelectedToggle] = useState(HOME_TOGGLE[0].id)
