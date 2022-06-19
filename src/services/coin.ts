405c1804989eb4a5e7d2eaf8deaf2f425a697505
HyeongSeoku <gudtjr3437@naver.com>
2022-06-20 02:33:35 +0900

    Feat : url change

--- src/services/coin.ts
+++ src/services/coin.ts
@@ -9,33 +9,33 @@ interface IHistoryParams {
 const BASE_URL = 'https://api.coinpaprika.com'
 
 export const getKeywordCoins = () =>
-  axios.get(`/v1/coins`).then((res) => {
+  axios.get(`${BASE_URL}/v1/coins`).then((res) => {
     const spliceRes = res.data.splice(0, 5000)
     const result: IDropDown[] = []
     spliceRes.map((item: ICoinData) => result.push({ name: item.name, id: item.id }))
     return result
   })
 
-export const getTop100CoinList = () => axios.get(`/v1/coins`).then((res) => res.data.splice(0, 100))
+export const getTop100CoinList = () => axios.get(`${BASE_URL}/v1/coins`).then((res) => res.data.splice(0, 100))
 
-export const getCoinList = () => axios.get(`/v1/coins`)
+export const getCoinList = () => axios.get(`${BASE_URL}/v1/coins`)
 
-export const getTop100CoinTickers = () => axios.get(`/v1/tickers`).then((res) => res.data.splice(0, 100))
+export const getTop100CoinTickers = () => axios.get(`${BASE_URL}/v1/tickers`).then((res) => res.data.splice(0, 100))
 
-export const getCoinTickers = () => axios.get(`/v1/tickers`).then((res) => res.data)
+export const getCoinTickers = () => axios.get(`${BASE_URL}/v1/tickers`).then((res) => res.data)
 
 export const getCoinDetailGraph = (params: IHistoryParams) =>
-  axios.get(`/v1/tickers/${params.coinId}/historical`, {
+  axios.get(`${BASE_URL}/v1/tickers/${params.coinId}/historical`, {
     params: {
       start: params.start,
       interval: params.interval,
     },
   })
 
-export const get300CoinTickers = () => axios.get(`/v1/tickers`).then((res) => res.data.splice(0, 300))
+export const get300CoinTickers = () => axios.get(`${BASE_URL}/v1/tickers`).then((res) => res.data.splice(0, 300))
 
 export const getSpecificCoinTicker = (name: string) =>
-  axios.get(`/v1/tickers`).then((res) => res.data.find((item: ITickerProps) => item.name === name))
+  axios.get(`${BASE_URL}/v1/tickers`).then((res) => res.data.find((item: ITickerProps) => item.name === name))
 
 export const getDetailData = async (coinName: string, apiStart: string) => {
   const coinDetail: ITickerProps = await getSpecificCoinTicker(coinName)
