a3745b628ed32373e2386bfac6c0d8e29fad61db
HyeongSeoku <gudtjr3437@naver.com>
2022-06-20 02:34:31 +0900

    Feat : Add home button on not found page

--- src/routes/NotFound404/index.tsx
+++ src/routes/NotFound404/index.tsx
@@ -1,4 +1,6 @@
 import { NotFound } from 'assets/svgs'
+import { Link } from 'react-router-dom'
+
 import styles from './notFound404.module.scss'
 
 const NotFoundPage = () => {
@@ -6,6 +8,11 @@ const NotFoundPage = () => {
     <div className={styles.notFoundContainer}>
       <h1 className={styles.title}>경로를 찾을수 없습니다.</h1>
       <NotFound className={styles.notFound} />
+      <Link to='/'>
+        <button type='button' className={styles.goHomeBtn}>
+          되돌아가기
+        </button>
+      </Link>
     </div>
   )
 }
